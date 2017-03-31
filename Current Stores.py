a14b947dcc68e370af6bc7b706620d9ef145413f
matthewwolf <Blade1234!>
2017-03-31 11:17:27 -0700

    creating new script for just output
    This is a script that will only output a new clean CSV with one record
    for each customer as well as their activation status, installation
    status and MRR

--- Current Stores.py
+++ Current Stores.py
@@ -21,8 +21,8 @@ stillInstalled = []
 stillActive = []
 # stores that are still installed but not active
 notActive = []
-
-
+# stores that are uninstalled
+uninstalled = []
 
 #A unique set of stores
 storeSet = set()
@@ -33,7 +33,7 @@ with open("apphistory.csv", 'rb') as csvfile:
     inputFile = csv.reader(csvfile, delimiter=",",quotechar="|")
 
     for row in inputFile:
-        #remove rows that are shopify employees or are our own test accounts
+        #re create the file here if we need it later the content is in memory.
         masterFile.append(row)
 
         # Format is 0-date, 1-event, 2-plan, 3-billing date, 4-store name, 5-store country, 6-contact, 7-store web
@@ -49,15 +49,18 @@ with open("apphistory.csv", 'rb') as csvfile:
 
 
 # ALL STORES THAT ARE CURRENTLY INSTALLED REGARDLESS OF ACTIVATION
-def currentlyinstalledstores():
+def currentlyinstalledstores(typeWanted):
     stillInstalled[:] =[]
 
     # look for any stores that installed and uninstalled multiple times and if there are more installs than uninstalls
     # keep them. If there are the same number of uninstalls as installs, remove it from the installed list
     for names in storeSet:
-        if installs.count(names) > uninstalls.count(names):
-            stillInstalled.append(names)
-
+        if typeWanted == "installed":
+            if installs.count(names) > uninstalls.count(names):
+                stillInstalled.append(names)
+        elif typeWanted == "uninstalled":
+            if installs.count(names) <= uninstalls.count(names):
+                uninstalled.append(names)
 
 
 # ALL CURRENTLY INSTALLED STORES THAT ARE CURRENTLY ACTIVATED
@@ -82,10 +85,10 @@ def currentactivations():
             if len(datesOfActivation) == 1:
                     stillActive.append(datesOfActivation[0])
             elif len(datesOfActivation) > 1:
-                stillActive.append(findnewestactivation(datesOfActivation))
+                stillActive.append(findnewestdate(datesOfActivation))
 
 # a function to find the most recent activation date in a list
-def findnewestactivation(myList):
+def findnewestdate(myList):
     newest = myList[0]
     counter = 0
 
@@ -137,6 +140,17 @@ def findstoretype(activeStores):
 
     return plans
 
+
+
+def createmasterfile():
+    cleanMasterFile =[]
+
+    return cleanMasterFile
+
+
+
+
+
 def writecsv(somelist, filename):
         filename = filename+".csv"
         with open(filename, 'a') as outcsv:
@@ -146,6 +160,9 @@ def writecsv(somelist, filename):
             for s in somelist:
                 #Write item to outcsv
                 writefile.writerow(s)
+
+
+
 
 
 
@@ -160,13 +177,13 @@ def summary():
 
 
 # Make sure everything gets populated and then ask the user what they want to do
-currentlyinstalledstores()
+currentlyinstalledstores("installed")
 
 
 while True:
     print ""
     print ""
-    print "Press 0 to Run the script through"
+    print "Press 0 to Create a Master File"
     print "Press 1 for Currently Installed stores"
     print "Press 2 for Stores that are currently activated"
     print "Press 3 for Installed but not active stores"
@@ -177,8 +194,71 @@ while True:
     userSelection = raw_input("Select an option: ")
     userSelection = int(userSelection)
 
+
+
     if userSelection == 0:
-        currentlyinstalledstores()
+        cleanMaster = []
+        dates = []
+
+
+        currentlyinstalledstores("installed")
+        for s in stillInstalled:
+            for x in masterFile:
+                if s == x[4]:
+                    if x[1] == "ApplicationInstalledEvent":
+                        appendMe = [x[4],x[2],x[0],x[7],x[6],"Installed"]
+                        dates.append(appendMe)
+
+                        if len(dates) > 1:
+                            cleanMaster.append(findnewestdate(dates))
+                        else:
+                            mostRecentInstall = dates
+                            cleanMaster.append(dates)
+
+            dates[:] = []
+
+                    ## Also see what their activation status is
+                    ## Also get their MRR
+
+
+
+        dates[:] = []
+        currentlyinstalledstores("uninstalled")
+        for s in uninstalled:
+            for x in masterFile:
+                if s == x[4]:
+                    if x[1] == "ApplicationUninstalledEvent":
+                        dates.append(x[4],x[2],x[0],x[7],x[6])
+
+                    mostRecentUninstall = findnewestdate(dates)
+                    cleanMaster.append(x[4],"Uninstalled",x[2],x[0],x[7],x[6])
+
+
+
+
+
+
+
+        # Create a file that has ALL stores, when they installed, when they activated, current activation status, montly payment, store name, store website, contact information
+        # Logic is to get all stores from masterFile, then use StoreSet to find the newest activation and cancellation of the store
+
+        #for store in storeSet:
+            # Determine if a store has been checked yet by comparing it to the storeSet
+            # If it has, skip it
+            # If it has not, get all instances of that store in masterFile
+            # Find all the installs and use findnewestdate to get the most recent
+            # find all the uninstalls and use findnewestdate to the most recent
+            # compare the two and whatever the newsest date is, set as the status
+            # if a store is still installed determine if the store is active by checking the stillActive List, if so set the status
+            # Get and set the plan they are on
+            # Get their Montly price
+            # search the masterFile for the fist instance of the store and then get the store address and contact information
+
+            # Format is 0-date, 1-event, 2-plan, 3-billing date, 4-store name, 5-store country, 6-contact, 7-store web
+
+
+    elif userSelection == 1:
+        currentlyinstalledstores("installed")
         pprint (stillInstalled)
         print "Total Stores Installed: " + str(len(stillInstalled))
 
@@ -207,7 +287,7 @@ while True:
 
 
     elif userSelection == 4:
-        currentlyinstalledstores()
+        currentlyinstalledstores("installed")
         currentactivations()
         notactivated()
         print 'TOTAL SUMMARY \n\n'
