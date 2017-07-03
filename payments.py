f2e10543b0a9c64e852ce3ba27e1dec461ec7462
matthewwolf <Blade1234!>
2017-07-03 01:05:07 -0700

    2 sheet excel file created
    A two sheet excel file is now created along with the csv. The Excel
    file has a sheet for the summary data and a sheet for the raw data for
    reference.

--- payments.py
+++ payments.py
@@ -1,5 +1,6 @@
 import csv
 from datetime import *
+import xlsxwriter
 
 
 # Global vars
@@ -43,7 +44,7 @@ for s in storeSet:
 # Create a final row of Totals
 output.append(['Totals', round(sumTotal, 2), sumPayments, '', ''])
 
-# Output the list to a file
+# Output the list to a csvfile
 with open("payments by store.csv", 'wb') as outcsv:
     # Configure writer to write standard csv file
     writefile = csv.writer(outcsv, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
@@ -51,6 +52,62 @@ with open("payments by store.csv", 'wb') as outcsv:
     for o in output:
         writefile.writerow(o)
 
+# Output the file to an Excel Spreadsheet
+workbook = xlsxwriter.Workbook('Payments from Stores2.xlsx')
+worksheet1 = workbook.add_worksheet('summary')
+
+# Create formatting
+money = workbook.add_format({'num_format': '$#,##0.00'})
+bold = workbook.add_format({'bold': True})
 
 
+# Create a sheet for the summarized data
+del o
+row = 0
+col = 0
+worksheet1.write(row, col, 'Store', bold)
+worksheet1.write(row, col + 1, 'Total Amount', bold)
+worksheet1.write(row, col + 2, 'Number of Payments', bold)
+worksheet1.write(row, col + 3, 'First Payment Date', bold)
+worksheet1.write(row, col + 4, 'Last Payment Date', bold)
+row += 1
+
+# Iterate over the data and write it out row by row.
+for o in (output):
+    for c in o:
+        worksheet1.write(row, col, o[0])
+        worksheet1.write(row, col + 1, o[1], money)
+        worksheet1.write(row, col + 2, o[2])
+        worksheet1.write(row, col + 3, o[3])
+        worksheet1.write(row, col + 4, o[4])
+    row += 1
+
+
+# Create a sheet for the raw data
+worksheet2 = workbook.add_worksheet('Raw Data')
+row = 0
+col = 0
+del m
+worksheet2.write(row, col, 'Payment Duration', bold)
+worksheet2.write(row, col + 1, 'Payment Date', bold)
+worksheet2.write(row, col + 2, 'Store', bold)
+worksheet2.write(row, col + 3, 'Charge Creation Time', bold)
+worksheet2.write(row, col + 4, 'Charge Type', bold)
+worksheet2.write(row, col + 5, 'Amount', bold)
+worksheet2.write(row, col + 4, 'App Title', bold)
+row += 1
+
+for m in masterList:
+    worksheet2.write(row, col, m[0])
+    worksheet2.write(row, col + 1, m[1])
+    worksheet2.write(row, col + 2, m[2])
+    worksheet2.write(row, col + 3, m[3])
+    worksheet2.write(row, col + 4, m[4])
+    worksheet2.write(row, col + 5, float(m[5]), money)
+    worksheet2.write(row, col + 6, m[6])
+    row += 1
+
+
+#Close the workbook
+workbook.close()
 
