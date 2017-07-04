2b6aa080928ba19026f3d7c8fe74efba779bdd4a
matthewwolf <Blade1234!>
2017-07-04 15:51:35 -0700

    added ability to create XLSX file
    CSV and XLS Files created, the XLSX will bold the top row (column
    headers) as well as automatically apply filters to the columns where
    data exists. Dates and currencies are also formatted (US date/currency
    format)
    
    Fixed some formatting

--- Convert Master.py
+++ Convert Master.py
@@ -1,4 +1,10 @@
+#!/usr/bin/env python
+
+
 import csv
+import xlsxwriter
+import string  # To help with the XLSX filtering
+from datetime import *
 
 # Declare global vars
 masterList = []
@@ -114,7 +120,7 @@ for store in storeSet:
             mostRecentExpired = None
 
         # Determine if the most recent event was accepting a charge and if so, how much was it. If not, then the store is not active
-        mostRecentEvent = isnewest([["Installed",mostRecentInstall], ["Accepted", mostRecentAccepted], ["Cancelled", mostRecentCancelled], ["Frozen", mostRecentFrozen], ["Expired", mostRecentExpired]])
+        mostRecentEvent = isnewest([["Installed", mostRecentInstall], ["Accepted", mostRecentAccepted], ["Cancelled", mostRecentCancelled], ["Frozen", mostRecentFrozen], ["Expired", mostRecentExpired]])
         if mostRecentEvent[0] == "Accepted":
             activeStatus = "Active"
             mrrAmount = str(t[2])[-6:].strip()
@@ -127,14 +133,86 @@ for store in storeSet:
         storeStatus = "Uninstalled"
 
     # Add the information for this store to the list of stores to output
-    cleanList.append([t[4], t[7], t[0], storeStatus, activeStatus, mrrAmount, t[6]])
+    cleanList.append([t[4], t[7], datetime.strptime(t[0], '%Y-%m-%d %H:%M:%S %Z'), storeStatus, activeStatus, mrrAmount, t[6]])
 
 
-# Output the list to a file
+# Output the list to a csv file
 with open("Store List.csv", 'wb') as outcsv:
     # Configure writer to write standard csv file
     writefile = csv.writer(outcsv, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
     for s in cleanList:
         writefile.writerow(s)
+
+
+
+# Output the file to an Excel Spreadsheet
+workbook = xlsxwriter.Workbook('Store List.xlsx', {'strings_to_numbers': True})
+worksheet1 = workbook.add_worksheet('Summary')
+thing = 'A1:' + string.uppercase[len(cleanList[0])-1] + '1'
+worksheet1.autofilter('A1:' + string.uppercase[len(cleanList[0])-1] + '1')
+
+# Create formatting
+money = workbook.add_format({'num_format': '$#,##0.00'})
+bold = workbook.add_format({'bold': True})
+dateFormat = workbook.add_format({'num_format': 'mm/dd/yy'})
+
+
+# Create a sheet for the summarized data
+row = 0
+col = 0
+
+worksheet1.write(row, col, 'Store Name', bold)
+worksheet1.write(row, col + 1, 'Store URL', bold)
+worksheet1.write(row, col + 2, 'Install Date', bold)
+worksheet1.write(row, col + 3, 'Install Status', bold)
+worksheet1.write(row, col + 4, 'Active Status', bold)
+worksheet1.write(row, col + 5, 'Current MRR', bold)
+worksheet1.write(row, col + 6, 'Contact Info', bold)
+row += 1
+
+
+# Iterate over the data and write it out row by row.
+for c in cleanList[1:]:
+    worksheet1.write(row, col, c[0].decode('utf-8').strip())
+    worksheet1.write(row, col + 1, c[1].decode('utf-8').strip())
+    worksheet1.write(row, col + 2, c[2], dateFormat)
+    worksheet1.write(row, col + 3, c[3].decode('utf-8').strip())
+    worksheet1.write(row, col + 4, c[4].decode('utf-8').strip())
+    worksheet1.write(row, col + 5, c[5], money)
+    worksheet1.write(row, col + 6, c[6].decode('utf-8').strip())
+    row += 1
+
+# Create a sheet for the raw data
+worksheet2 = workbook.add_worksheet('Raw Data')
+worksheet2.autofilter('A1:' + string.uppercase[len(masterList[0])-1] + '1')
+
+
+row = 0
+col = 0
+
+worksheet2.write(row, col, 'Date', bold)
+worksheet2.write(row, col + 1, 'Event', bold)
+worksheet2.write(row, col + 2, 'Details', bold)
+worksheet2.write(row, col + 3, 'Billing on', bold)
+worksheet2.write(row, col + 4, 'Shop name', bold)
+worksheet2.write(row, col + 5, 'Shop country', bold)
+worksheet2.write(row, col + 6, 'Shop email', bold)
+worksheet2.write(row, col + 7, 'Shop domain', bold)
+row += 1
+
+for m in masterList:
+    worksheet2.write(row, col, m[0], dateFormat)
+    worksheet2.write(row, col + 1, m[1].decode('utf-8').strip())
+    worksheet2.write(row, col + 2, m[2].decode('utf-8').strip())
+    worksheet2.write(row, col + 3, m[3], dateFormat)
+    worksheet2.write(row, col + 4, m[4].decode('utf-8').strip())
+    worksheet2.write(row, col + 5, m[5].decode('utf-8').strip())
+    worksheet2.write(row, col + 6, m[6].decode('utf-8').strip())
+    worksheet2.write(row, col + 7, m[7].decode('utf-8').strip())
+    row += 1
+
+
+#Close the workbook
+workbook.close()
 
 
